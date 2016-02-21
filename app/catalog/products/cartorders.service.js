10bc76f6762f984327df867a777ac9121231ba74
Brock Clayton <info@bluerockapps.com>
2016-02-21 14:07:15 -0700

    master update

--- app/catalog/products/cartorders.service.js
+++ app/catalog/products/cartorders.service.js
@@ -11,17 +11,21 @@ app.factory('CartOrders', ['$firebaseArray', '$firebaseObject', 'FirebaseUrl', '
         });
       },
 
+      updateCart: function(theObj) {
+        var theRef = new Firebase(FirebaseUrl+'carts/'+tid+'/'+theObj.cid);
+        return theRef.update( {items: theObj.items, total: theObj.total, update_date: Firebase.ServerValue.TIMESTAMP} );
+      },
+
       /* product ID is used as node ID (theObj.$id) on order array */
       addProduct: function(theObj) {
-        var theRef = new Firebase(FirebaseUrl+'orders/'+tid+'/'+theObj.orderId+'/'+theObj.$id);
+        var theRef = new Firebase(FirebaseUrl+'orders/'+tid+'/'+theObj.oid+'/'+theObj.$id);
         theRef.update( {product_id: theObj.$id, product_name: theObj.product_name, product_regular_price: theObj.product_price,
           product_quantity: theObj.product_quantity, product_special_price: theObj.special_price, product_image: theObj.product_image,
           order_status: 'cart', order_update_date: Firebase.ServerValue.TIMESTAMP} );
-        cartorder.updateCart(theObj);
       },
 
       nextProduct: function(theObj) {
-        var data = $firebaseArray(ref.child(tid).child(theObj.orderId));
+        var data = $firebaseArray(ref.child(tid).child(theObj.oid));
         data.$loaded().then(function() {
           cnt = 1;
           for(i = 0; i < data.length; i++) {
--- app/catalog/products/featured.controller.js
+++ app/catalog/products/featured.controller.js
@@ -1,5 +1,5 @@
-app.controller('CatalogFeaturedCtrl', ['$state', 'CartOrders', 'Products', '$scope', '$stateParams', '$cookies',
-  function (                            $state,   CartOrders,   Products,   $scope,   $stateParams,   $cookies) {
+app.controller('CatalogFeaturedCtrl', ['$state', 'Catalog', 'CartOrders', 'Products', '$scope', '$stateParams', '$cookies',
+  function (                            $state,   Catalog,   CartOrders,   Products,   $scope,   $stateParams,   $cookies) {
     var catalogFeaturedCtrl = this;
     catalogFeaturedCtrl.featuredProducts = Products.allFeatured;
 
@@ -12,19 +12,29 @@ app.controller('CatalogFeaturedCtrl', ['$state', 'CartOrders', 'Products', '$sco
       theProduct.$loaded().then(function() {
         if (theProduct.product_special_price === undefined)
           theProduct.special_price = null;
-        theProduct.cart_id = $cookies.get('cartId');
-        var theOrderId = $cookies.get('orderId');
-        if (theOrderId === undefined) {
+        var cid = $cookies.get('cartId');
+        var oid = $cookies.get('orderId');
+        if (oid === undefined) {
           CartOrders.addOrder().then(function(theRef) {
-            theProduct.orderId = theRef;
+            theProduct.oid = theRef;
             theProduct.product_quantity = 1;
             $cookies.put("orderId", theRef);
             CartOrders.addProduct(theProduct);
           });
         } else {
-          theProduct.orderId = theOrderId;
+          theProduct.oid = oid;
           CartOrders.nextProduct(theProduct);
         }
+        var theCart = Catalog.getCart(cid);
+        theCart.$loaded().then(function() {
+          theCart.items = theCart.items + 1;
+          if (theProduct.special_price != null)
+            theCart.total = theCart.total + theProduct.special_price;
+          else
+            theCart.total = theCart.total + theProduct.product_price;
+          theCart.cid = cid;
+          CartOrders.updateCart(theCart);
+        });
       });
     };
 
