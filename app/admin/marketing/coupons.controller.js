15ca2557e62034252a3d11f3240bcd31bd798821
cecilisrael <cecil@bluerockapps.com>
2016-02-23 17:13:48 -0700

    Coupons now sending data to firebase

--- app/admin/marketing/coupons.controller.js
+++ app/admin/marketing/coupons.controller.js
@@ -1,20 +1,43 @@
 app.controller('CouponsCtrl', ['Coupons', '$state', '$scope', '$stateParams',
   function (                    Coupons,   $state,   $scope,   $stateParams) {
     var couponsCtrl = this;
+    couponsCtrl.allCoupons = Coupons.all;
 
+//called from coupons.html
+    couponsCtrl.addCoupon = function() {
+//console test to check if it's working
+    console.log('gothere')
+    var theCoupon = {};
+    theCoupon.couponName = couponsCtrl.coupon_name;
+    theCoupon.couponDiscount = couponsCtrl.coupon_discount;
+//calling coupons service addCoupon
+    Coupons.addCoupon(theCoupon);
+  }, function(error) {
+    couponsCtrl.error = error;
+  };
+
+//Coupons Grid Options
     couponsCtrl.gridCoupons = {
       enableSorting: true,
       enableCellEditOnFocus: true,
       data: Coupons.all,
       columnDefs: [
-        { name: '', field: '$id', shown: false, cellTemplate: 'admin/catalogs/gridTemplates/editSubCategoriesBtn.html',
-          width: 34, enableColumnMenu: false, enableCellEdit: false, headerTooltip: 'Add Sub-categories' },
-        { name:'categoryName', field: 'category_name', width: '70%', enableHiding: false },
-        { name:'menuOrder', field: '$priority', enableHiding: false },
-        { name:'subCount', field: 'sub_count', visible: false },
-        { name: ' ', field: '$id', cellTemplate:'admin/catalogs/gridTemplates/removeCategoryBtn.html',
-          width: 32, enableCellEdit: false, enableColumnMenu: false }
+        { name:'coupon', field: 'coupon_name', width: '60%', enableHiding: false },
+        { name:'discount', field: 'coupon_discount', width: '35%', enableHiding: false },
+        { name: ' ', field: '$id', cellTemplate:'admin/marketing/gridTemplates/removeCoupon.html',
+          width: 50, enableCellEdit: false, enableColumnMenu: false }
       ]
+    };
+//called from removeCoupon.html
+    couponsCtrl.removeCoupon = function(row) {
+//console test to check if it's working
+    console.log('gothere')
+      var theCoupon = {};
+      theCoupon.couponName = couponsCtrl.coupon_name;
+      theCoupon.couponDiscount = couponsCtrl.coupon_discount;
+      Coupons.removeCoupon(theCoupon);
+    }, function(error) {
+      productCtrl.error = error;
     };
 
 }]);
