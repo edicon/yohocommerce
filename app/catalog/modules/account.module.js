d6950555d9a6c5d2cdfd42f32d33c26805db83b2
cecilisrael <cecil@bluerockapps.com>
2016-03-24 13:31:56 -0600

    fixed account module discrepancy

--- app/catalog/modules/account.module.js
+++ app/catalog/modules/account.module.js
@@ -271,12 +271,10 @@ angular.module('AccountModule', [
 
           var theGiftCard = GiftCard.getGiftCard(profile);
               theGiftCard.$loaded().then(function() {
-<<<<<<< HEAD
-              accountGiftCardCtrl.gridGiftCards.data = theGiftCard;
-=======
+
                 accountGiftCardCtrl.gridGiftCards.data = theGiftCard;
 
->>>>>>> master
+
           });
 
           accountGiftCardCtrl.gridGiftCards = {
